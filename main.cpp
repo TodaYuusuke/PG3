350048a773eb60b87b4178005c03c6f33a4fd4f6
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2023-10-02 12:46:04 +0900

    printf消去

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 
 template <typename T>
-T Min(T a, T b) {
-	return a < b ? a : b;
+void Min(T a, T b) {
+	T value = a < b ? a : b;
+	std::cout << value << std::endl;
 }
 template <>
-char Min<char>(char a, char b) {
-	printf("数字以外は代入できません\n");
-	return '\0';
+void Min<char>(char a, char b) {
+	std::cout << "数字以外は代入できません" << std::endl;
 }
 
 int main() {
-	int a(1), b(2);
-	float c(1.2f), d(0.8f);
-	double e(3.2), f(4.1);
-	char g('a'), h('d');
-
-	printf("int型の比較 Min(%d, %d) -> ", a, b);
-	printf("%d\n", Min<int>(a, b));
-	printf("float型の比較 Min(%.2f, %.2f) -> ", c, d);
-	printf("%.2f\n", Min<float>(c, d));
-	printf("double型の比較 Min(%.2lf, %.2lf) -> ", e, f);
-	printf("%.2lf\n", Min<double>(e, f));
-	printf("char型の比較 Min(%c, %c) -> ", g, h);
-	printf("%c\n", Min<char>(g, h));
-
+	Min(1, 2);
+	Min(1.2f, 0.8f);
+	Min(3.2, 4.1);
+	Min('a', 'b');
+	
 	return 0;
 }
