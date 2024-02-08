bd4f0c1074d7527a5baffa5d83edba8dc977c49b
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-02-08 15:33:19 +0900

    移動とコピーの比較完了

--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
 #include <stdio.h>
-#include <thread>
+#include <string>
+#include <chrono>
 
-void Print(int i) {
-	printf("thread %d\n", i); 
+// 経過時間を取得する
+void CheckElapsedTime(const std::chrono::steady_clock::time_point& startTime) {
+	std::chrono::duration<double, std::milli> elapsedTime = std::chrono::steady_clock::now() - startTime;
+	printf("かかった時間は%lfマイクロ秒です。\n", elapsedTime.count());
 }
 
 int main() {
-	std::thread th1(Print, 1);
-	th1.join();
-	std::thread th2(Print, 2);
-	th2.join();
-	std::thread th3(Print, 3);
-	th3.join();
+	// 10万文字の文字列
+	std::string a(100000, 'a');
+	// 実行開始時間
+	std::chrono::steady_clock::time_point startTime;
+
+	printf("「a」10万文字をコピーします\n");
+	// 計測開始
+	startTime = std::chrono::steady_clock::now();
+	// コピー
+	std::string b = a;
+	// 経過時間を測定
+	CheckElapsedTime(startTime);
+
+
+	printf("「a」10万文字を移動します\n");
+	// 計測開始
+	startTime = std::chrono::steady_clock::now();
+	// コピー
+	std::string c = std::move(a);
+	// 経過時間を測定
+	CheckElapsedTime(startTime);
 }
